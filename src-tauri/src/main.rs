14cb164ff9f0d979a88afed1f0a027ac0994c8fe
Allen <allen.davisswing@rutgers.edu>
2022-06-12 10:57:32 -0400

    opening files

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,26 +1,69 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use pulldown_cmark::{Parser, Options, html};
+use native_dialog::{FileDialog };
+use pulldown_cmark::{html, Options, Parser};
+use std::{fs::File, io::{Read, Write}, path::{PathBuf, Path}, str::FromStr};
 
+#[derive(serde::Serialize)]
+struct FileMessage {
+  contents: String,
+  path: String,
+}
 
 #[tauri::command]
 fn parse_md(input: String) -> String {
-  let mut options = Options::empty();
-  options.insert(Options::ENABLE_TABLES);
-  let parser = Parser::new_ext(&input, options);
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_TABLES);
+    let parser = Parser::new_ext(&input, options);
 
-  let mut html_output = String::new();
-  html::push_html(&mut html_output, parser);
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
 
-  html_output
+    html_output
+}
+
+#[tauri::command]
+fn load_file() -> FileMessage {
+    let path = FileDialog::new()
+        .set_location("~/Documents")
+        .add_filter("Markdown File", &["md"])
+        .show_open_single_file()
+        .unwrap();
+
+    let path = match path {
+        Some(path) => path,
+        None => return FileMessage {
+            contents: "".to_string(),
+            path: "".to_string()
+        },
+    };
+
+    let mut f = File::open(&path).unwrap();
+    let mut result = String::new();
+    f.read_to_string(&mut result);
+
+    FileMessage { contents: result, path: path.to_str().unwrap().to_string() }
+}
+
+#[tauri::command]
+fn save_file(updated_contents: String, path: String) {
+    let path = PathBuf::from_str(&path);
+    let path = match path {
+        Ok(path) => path,
+        Err(_) => return,
+    };
+
+    let mut file = File::open(&path).unwrap();
+    file.write(updated_contents.as_bytes());
+
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![parse_md])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![parse_md, load_file])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,52 @@
 import React, { useRef } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { useEditable } from "use-editable";
+import { appWindow, WebviewWindow } from '@tauri-apps/api/window'
 import "./App.css";
 
+type FileMessage = {
+  contents: string,
+  path: string,
+}
+
 function App() {
-  const [text, setText] = React.useState("Starting point");
+  const [text, setText] = React.useState("");
+  const [path, setPath] = React.useState("")
   const [markdown, setMarkdown] = React.useState("");
+
+  appWindow.emit('event', { message: 'Tauri is awesome!' })
+
+  React.useEffect(() => {
+    invoke("parse_md", { input: text }).then((result: any) => {
+      setMarkdown(result)
+    })
+  }, [text]);
+
+  React.useEffect(() => {
+    // loading the file that we have selected
+    var file_contents: string = ""
+    invoke("load_file").then((result: any) => {
+      file_contents = result.contents
+      setText(file_contents)
+      setPath(result.path)
+    })
+  }, [])
 
   const editorRef = useRef(null);
   useEditable(editorRef, setText);
 
-  React.useEffect(() => {
-    invoke("parse_md", { input: text }).then((result: any) => {
-      setMarkdown(result);
-    });
-  }, [text]);
-
   return (
     <div className="App">
       <body>
+        <div>{path}</div>
         <div className="container">
-          <textarea ref={editorRef}>{text}</textarea>
-          <div dangerouslySetInnerHTML={{ __html: markdown }} />
+          <div ref={editorRef} className="textHolder">
+            {text}
+          </div>
+          <div
+            dangerouslySetInnerHTML={{ __html: markdown }}
+            className="previewHolder"
+          />
         </div>
       </body>
     </div>
